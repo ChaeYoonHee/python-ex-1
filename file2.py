583d07c0b71c5413d2aad498a24fda456cdd6c08
ChaeYoonHee <cyhi03033@naver.com>
2024-06-16 17:56:51 +0900

    숙제

---
+++ file2.py
@@ -0,0 +1,11 @@
+first=float(input('첫번째 실수를 입력하세요 >>>'))
+second=float(input('두번째 실수를 입력하세요 >>>'))
+
+sum = first + second
+
+print('{}와 {}의 합은 {}입니다.'.format(first, second, sum))
+
+month=(input('1~12 사이의 월을 입력하세요 >>>'))
+list = [31,28,30]
+
+print('{}은 {}일까지 있습니다.' .format(month, list))
