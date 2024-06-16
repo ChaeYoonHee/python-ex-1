3232939e0e19c1cdd1ea391fe64d64dd80d98ac9
ChaeYoonHEe <cyhi03033@naver.com>
2024-06-16 20:22:03 +0900

    숙제

--- file2.py
+++ file2.py
@@ -5,7 +5,23 @@ sum = first + second
 
 print('{}와 {}의 합은 {}입니다.'.format(first, second, sum))
 
+
 month=(input('1~12 사이의 월을 입력하세요 >>>'))
 list = [31,28,30]
 
 print('{}은 {}일까지 있습니다.' .format(month, list))
+
+
+word = input('영어 단어를 입력하세요 >>>')
+
+dict = {'flower':'꽃', 'fly': '날다', 'floor':'바닥'}
+print('{}:{}'.format(word, dict[word]))
+
+
+traver = set()
+
+travel = input('희망하는 수학여행지를 입력하세요 >>>')
+travel = input('희망하는 수학여행지를 입력하세요 >>>')
+travel = input('희망하는 수학여행지를 입력하세요 >>>')
+
+print('조사된 수학 여행지는 {} 입니다'.format(travel))
