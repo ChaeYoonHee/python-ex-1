3eddecc08857c66c299efebddf58351fdd1955f3
ChaeYoonHee <cyhi03033@naver.com>
2024-06-22 17:56:32 +0900

    숙제

---
+++ file4.py
@@ -0,0 +1,28 @@
+score = int(input('점수를 입력하세요 >>>'))
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else:
+    print('F')
+
+
+
+number = int(input('정수를 입력하세요'))
+
+if number % 3 == 0 :
+    print('{}는 3의 배수입니다.'.format(number)) 
+else:
+    print('{}는 3의 배수가 아닙니다'.format(number))  
+
+
+num_1 = int(input('정수1 입력'))
+num_2 = int(input('정수2 입력'))
+num_3 = int(input('정수3 입력'))
+
+
